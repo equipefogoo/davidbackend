ad55d3b8844a6ecc5128a6ad2eb805d682ed20f4
seundo A <david070931@gmail.com>
2026-04-14 08:26:47 -0300

    Add project summary and details to index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+/******************************************************************************
+Resumo do seu projeto de Node.js
+*******************************************************************************/
+console.log ('Escola do Futuro do Estado de  Goías  -  José Luiz Bittencourt');
+console.log('Curso  Técnico de Front-End e Cibersegurança');
+console.log('Back-End');
+consol.log('George Mendes Marra');
+console.log('Sistema de Cadastro Simples CRUD Básico');
+console.log( 'Carlos Daniel Barbosa, Carlos Daniel Xavier, Davi Regis, David Aguiar');
+console.log('13/04/2026');
